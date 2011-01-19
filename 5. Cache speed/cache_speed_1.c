6293a6056449660994bd4f6bb301dfd21013dd31
s0151 <s0151@um32.imm.uran.ru>
2011-01-19 15:52:11 +0500

    Some fixes in 5th problem.

--- 5. Cache speed/cache_speed_1.c
+++ 5. Cache speed/cache_speed_1.c
@@ -3,6 +3,7 @@
 #define N 1500
 
 int main() {
+	FILE *result = fopen("result.txt", "w");
 	int a[N][N];
 	int i, j, devnull;
 
@@ -11,17 +12,17 @@ int main() {
 
 	for(i = 0; i < N; ++i)
 		for(j = 0; j < N; ++j)
-			devnull = a[i][j];
+			printf("%i", a[i][j]);
 
-	printf("1: %.3f\n", (float)(clock() - start) / CLOCKS_PER_SEC);
+	fprintf(result, "1: %.3f\n", (float)(clock() - start) / CLOCKS_PER_SEC);
 
 	start = clock();
 
         for(i = 0; i < N; ++i)
                 for(j = 0; j < N; ++j)
-                        devnull = a[j][i];
+                        printf("%i", a[j][i]);
 
-        printf("2: %.3f\n", (float)(clock() - start) / CLOCKS_PER_SEC);
+        fprintf(result, "2: %.3f\n", (float)(clock() - start) / CLOCKS_PER_SEC);
 
 	return 0;
 }
