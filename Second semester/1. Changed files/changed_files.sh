7939ce82297a6414ef2aa6374a0c7784ae578a2c
h15 <h15@home.(none)>
2011-03-12 21:08:28 +0500

    1st problem solved

--- Second semester/1. Changed files/changed_files.sh
+++ Second semester/1. Changed files/changed_files.sh
@@ -3,14 +3,14 @@
 #
 #   Make actual hash
 #
-function __read_fs {
-    for filename in `find $1 -type f | sort`
+function read_fs {
+    for filename in `find "$1" -type f | sort`
     do
-        filename=`readlink -f $filename`
+        filename=`readlink -f "$filename"`
         
-        size=`stat -c "%s" $filename`
+        size=`stat -c "%s" "$filename"`
         
-        md5=`md5sum $filename`
+        md5=`md5sum "$filename"`
         md5=${md5:0:32}
         
         md5_hash_current["$md5-$size"]+="$filename$n"
@@ -20,7 +20,7 @@ function __read_fs {
 #
 #   Write actual hash into file $dest
 #
-function __write_file {
+function write_file {
     echo -n > $dest
 
     for key in ${!md5_hash_current[@]}
@@ -32,13 +32,14 @@ function __write_file {
 #
 #   Read hash file into md5_hash_file (associative array)
 #
-function __read_hash_from_file {
+function read_hash_from_file {
     isNewCell=1
     key=''
-    
-    cat $dest | while read line
+
+    exec 9<$dest
+
+    while read -u9 line
     do
-        echo -n '.'
         if [[ $isNewCell -eq 1 ]]
         then
             key=$line
@@ -46,7 +47,7 @@ function __read_hash_from_file {
             continue
         fi
         
-        if [[ $line -eq $'' ]]
+        if [[ ${#line} -eq 0 ]]
         then
             isNewCell=1
             continue
@@ -59,19 +60,49 @@ function __read_hash_from_file {
 #
 #   diff md5_hash_file and md5_hash_current
 #
-function __check {
+function check_hashes {
+    isChanged=0
+    
+    # No white space here!
+    SAVE_IFS=$IFS
+    IFS=$'\n'
+
     for key in ${!md5_hash_file[@]}
     do
-        ${md5_hash_current[$key]} | while read line
+        for line in ${md5_hash_file[$key]}
         do
-            echo $line
+            # find substring $line in ${md5_hash_current[$key]}
+            echo ${md5_hash_current[$key]} | grep $line >/dev/null 2>&1
+            if [ "$?" != "0" ]
+            then
+                echo ">>> $line"
+                isChanged=1
+            fi
         done
+    done
+    
+    IFS=$SAVE_IFS
+    
+    if [ $isChanged = 0 ]
+    then
+        echo ">>> No files changed."
+    fi
+}
+
+#
+#   For debug
+#
+function print_hash {
+    for key in ${!md5_hash_file[@]}
+    do
+        echo "$key$n${md5_hash_file[$key]}"
     done
 }
 
 #
 #   Main
 #
+
 declare -A md5_hash_file
 declare -A md5_hash_current
 
@@ -82,19 +113,16 @@ shift
 dest=$1
 shift
 
-#__read_hash_from_file
-#exit
-
 case $option in
     '-make')
-        echo '[!] Reading file system...'; __read_fs $*
-        echo '[!] Write dump into file.' ; __write_file
+        echo '[!] Reading file system...'; read_fs $*
+        echo '[!] Write dump into file.' ; write_file
         echo '[+] Done!'
         ;;
     '-check')
-        echo '[!] Reading dump file...'  ; __read_hash_from_file
-        echo '[!] Reading file system...'; __read_fs $*
-        echo '[!] Compare dump with actual state.'; __check
+        echo '[!] Reading dump file...'  ; read_hash_from_file
+        echo '[!] Reading file system...'; read_fs $*
+        echo '[!] Compare dump with actual state.'; check_hashes
         echo '[+] Done!'
         ;;
     *)
