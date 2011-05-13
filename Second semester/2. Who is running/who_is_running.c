af6cddd336f7de4543d5ca9c80f05a04a625362b
h15 <h15@home.(none)>
2011-05-14 01:28:55 +0600

    no printf in signal

--- Second semester/2. Who is running/who_is_running.c
+++ Second semester/2. Who is running/who_is_running.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
+
+int p[2];
 
 void sigint( int signo ) {
-	pid_t pid = getpid();
-	gid_t gid = getgid();
-
-	fprintf( stderr, "\npid: %u;\ngid: %u;\n", pid, gid );
+    write(p[1], "a", 1);
 }
 
 int main( int argc, char** argv ) {
+    if ( pipe(p) == -1 ) {
+        fprintf( stderr, "Pipe init failed!\n" );
+        return 1;
+    }
+    
 	struct sigaction action, old_action;
-	
 	action.sa_handler = sigint;
-	
 	sigaction( SIGINT, &action, &old_action );
+    
+    volatile char counter = 0;
+    int size = 0;
+    
+    while( counter < 50 ) {
+        usleep(100000);
+        ioctl(p[0], FIONREAD, &size);
 
-	if ( argc > 0 )
-		sleep( (int)argv[0][0] );
-	else
-		sleep(5);
-
+        if ( size > 0 ) {
+            fprintf( stderr, "\npid: %d;\ngid: %d;\n", getpid(), getgid() );
+            break;
+        }
+        
+        ++counter;
+    }
+    
 	sigaction( SIGINT, &old_action, NULL );
 
+    close(p[0]);
+    close(p[1]);
+    
 	return 0;
 }
