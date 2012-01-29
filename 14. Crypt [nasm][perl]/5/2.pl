9e4dd376370964f43d8070fce0f047bc156b7580
Georgy Bazhukov <georgy.bazhukov@gmail.com>
2012-01-29 11:39:17 +0600

    add crypto broadcast

---
+++ 14. Crypt [nasm][perl]/5/2.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/env perl
+
+$S[1] = '1';
+$S[3] = '3';
+
+my $flag = shift @ARGV;
+
+put() if $flag eq 'write';
+get() if $flag eq 'read';
+
+sub put
+    {
+        my $line = <>;
+        chomp $line;
+        
+        $line =~ m/(.*?):(.*)$/s;
+        my ( $users, $text ) = ( $1, $2 );
+        
+        for my $u ( split ',', $users )
+        {
+            return unless defined $S[$u];
+            
+            my $crypt;
+            $crypt .= $S[$u] ^ substr $text, $_, 1 for 0 .. (length $text) - 1;
+            
+            $text = $crypt;
+        }
+        
+        open F, '>share.dat' or die;
+        print F "$users:$text";
+        close F;
+    }
+
+sub get
+    {
+        open F, 'share.dat' or die;
+        my $line = <F>;
+        close F;
+        
+        chomp $line;
+        
+        $line =~ /(.*?):(.*)$/s;
+        my ( $users, $text ) = ( $1, $2 );
+        
+        for my $u ( reverse split ',', $users )
+        {
+            return unless defined $S[$u];
+            
+            my $crypt;
+            $crypt .= $S[$u] ^ substr $text, $_, 1 for 0 .. (length $text) - 1;
+            
+            $text = $crypt;
+        }
+        
+        print "$users:$text\n";
+    }
